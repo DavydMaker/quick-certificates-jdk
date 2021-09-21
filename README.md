ec4644d385d0719e4694d5c9be12da72d856f311
Davyd Maker <davydmaker@gmail.com>
2021-09-20 22:58:19 -0300

    Correção bug de caminho do diretório com espaço quebrando comando

--- README.md
+++ README.md
@@ -2,19 +2,17 @@
 ##### Script to facilitate the import of various certificates for JDK cacerts.
 [![Shell Script](https://img.shields.io/badge/shell-script-blue)]()
 
-Ao executar o script ele vai importar no cacerts da JDK informada os certificados do diretório informado, utlizando o nome do arquivo como alias dele e sempre utilizando a senha `changeit`.
+Ao executar o script ele vai importar no cacerts da JDK informada os certificados do diretório informado, utlizando o nome do arquivo como alias dele e sempre utilizando a senha `changeit` (caso quiser alterar só mudar variável no script).
 
 ---
 
 ## Configurações
 Faça o apontamento do diretório da JDK alterando a variável `pathJava`. Exemplo:
 ```sh
-pathJava="C:/Program Files/Java/jdk-11.0.9"
+pathJava="C:\Program Files\Java\jdk-11.0.9"
 ```
 
 Faça o apontamento do diretório que contém a lista de certificados alterando a variável `pathCertificate` para importar para o cacerts da JDK informada. Exemplo:
 ```sh
-pathCertificate="C:/certificates"
+pathCertificate="C:\certificates"
 ```
-
-**Em ambos casos utilizar `/` como separador de diretório (assim como no exemplo).**
--- quick-certificates-jdk.sh
+++ quick-certificates-jdk.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 #title          :quick-certificates-jdk.sh
-#description    :Script to facilitate the import of various certificates for JDK cacerts.
+#description    :Script to facilitate the import of various certificates for JDK cacerts.   
 #author		    :davydmaker
-#date           :20210527
-#version        :1.1    
+#date           :20210910
+#version        :1.2    
 #usage		    :bash mkscript.sh
-#notes          :Indicate the JDK directory and the directory with the certificates to install (using "/").
+#notes          :Indicate the JDK directory and the directory with the certificates to install.
 #==============================================================================
 
-pathJava="C:/Program Files/Java/jdk-11.0.9"
-pathCertificate="C:/certificates"
+pathJava="C:\Program Files\Java\jdk-11.0.9"
+pathCertificate="C:\certificates"
 
 javaVersion=$("$pathJava/bin/java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
 pass="changeit"
 
-if [ $javaVersion > "8" ]; then cacerts="-cacerts"; else cacerts="-keystore $pathJva/lib/security/cacerts"; fi
+if [ $javaVersion > "8" ]; then cacerts="-cacerts"; else cacerts="-keystore \"$pathJava/lib/security/cacerts\""; fi
 
 for fileCrt in "$pathCertificates"/*.{crt,cer}
 do
-    fileCrtName=$(basename $fileCrt)
-    if [ $fileCrtName != "*.crt" ] && [ $fileCrtName != "*.cer" ]
+    fileCrtName=$(basename "$fileCrt")
+    if [ "$fileCrtName" != "*.cer" ] && [ "$fileCrtName" != "*.crt" ]
     then
         fileCrtName=${fileCrtName%.*}
-        eval "$pathJava/bin/keytool.exe -delete -noprompt -trustcacerts -alias $fileCrtName $cacerts -storepass $pass"
-        eval "$pathJava/bin/keytool.exe $cacerts -J-Duser.language=en -importcert -alias $fileCrtName -file $fileCrt -storepass $pass -trustcacerts -noprompt"
+        eval "\"$pathJava/bin/keytool.exe\" -delete -noprompt -trustcacerts -alias \"$fileCrtName\" $cacerts -storepass $pass"
+        eval "\"$pathJava/bin/keytool.exe\" $cacerts -J-Duser.language=en -importcert -alias \"$fileCrtName\" -file \"$fileCrt\" -storepass $pass -noprompt"
     fi
 done
